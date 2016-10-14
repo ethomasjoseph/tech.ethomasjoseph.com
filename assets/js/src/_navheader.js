1d3c5591390edc7949ae3666966f3c9915044b24
Thomas Joseph <ethomas.joseph@gmail.com>
2016-10-14 01:10:23 -0400

    Improvements in search bar and the collapsible Menu in mobile mode

--- assets/js/src/_navheader.js
+++ assets/js/src/_navheader.js
@@ -7,17 +7,19 @@
 var tjs = window.tjs || {};
 
 // self initializing function
-tjs.navheader = (function(context, $){
-    var _init,
+tjs.navheader = (function(context, $) {
+    var $searchWrapper,
+        _init,
+        _handlePageScroll,
+        _toggleSearchNCollapseNav,
         _initSearchButton;
-    
+
     _initSearchButton = function() {
         var searchIcons = $(".search-icon");
-        var searchWrapper = $("#search-wrapper");
-        $(searchIcons).click(function(){
-            $(searchWrapper).toggle(80, function(){
-                var searchOpen = $(searchWrapper).is(":visible");
-                $.each($(".search-icon"), function(i,el){
+        $(searchIcons).click(function() {
+            $($searchWrapper).toggle(80, function() {
+                var searchOpen = $($searchWrapper).is(":visible");
+                $.each($(".search-icon"), function(i, el) {
                     if (searchOpen) {
                         $(el).addClass("active-text");
                     } else {
@@ -25,15 +27,42 @@ tjs.navheader = (function(context, $){
                     }
                 });
                 if (searchOpen) {
+                    $('#tj-navbar-collapse').collapse('hide');
                     $("#txtSearch").focus();
                 }
             });
         });
     };
-    
-    _init = function() {
-        _initSearchButton();
+
+    _handlePageScroll = function() {
+        $(document).on("scroll", function() {
+            if ($(document).scrollTop() > 200) {
+                $("#mainNav").addClass("shrink");
+            } else {
+                $("#mainNav").removeClass("shrink");
+            }
+        });
     };
-    
+
+    _toggleSearchNCollapseNav = function() {
+        $('#tj-navbar-collapse').on('show.bs.collapse', function(e) {
+            var searchOpen = $($searchWrapper).is(":visible");
+            if (searchOpen) {
+                // gracefully hide the search bar
+                var searchIcon = $(".search-icon").first();
+                if (searchIcon.length) {
+                    searchIcon.trigger('click');
+                }
+            }
+        });
+    };
+
+    _init = function() {
+        $searchWrapper = $("#search-wrapper");
+        _initSearchButton();
+        _handlePageScroll();
+        _toggleSearchNCollapseNav();
+    };
+
     _init();
 }(window, jQuery));
